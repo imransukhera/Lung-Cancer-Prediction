d49cefd39a68b7df36d56dcc6c1c6b9ac1d06615
Muhammad Imran <imrange736@gmail.com>
2026-01-16 11:35:33 -0500

    learn sklearn.model_selection

--- pandas/write_csv.py
+++ pandas/write_csv.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
 
 from sklearn.model_selection import train_test_split
+
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
