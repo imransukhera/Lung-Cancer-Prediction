e9089a801eb3913f841b3bf1c94c898f2fa1192f
Muhammad Imran <imrange736@gmail.com>
2026-01-16 11:35:00 -0500

    learn matplotlib.patches

--- pandas/write_csv.py
+++ pandas/write_csv.py
@@ -5,6 +5,7 @@ import seaborn as sns
 
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
